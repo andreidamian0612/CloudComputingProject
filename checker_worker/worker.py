6cc2331ab424b2f0e744a505f4cba5b2ad42aaa3
andrei-damian <damadama1>
2020-01-13 01:20:48 +0200

    fix docker

--- checker_worker/worker.py
+++ checker_worker/worker.py
@@ -5,7 +5,7 @@ import database as db
 
 
 upload_path = '/usr/upload'
-worker_path = '/usr/upload'
+worker_path = '/usr/worker'
 checker_environment = '/usr/src/worker/checker_environment'
 
 homework_file = 'tema3'
@@ -14,7 +14,7 @@ checker_script = ''
 from_master = 'path'
 
 def start_worker(foldername):
-    os.system("cp -r /usr/src/worker/checker_environment ")
+    os.system("cp -r /usr/src/worker/checker_environment")
     student_name = foldername.split('_')[0]
     for file in os.listdir(foldername):
         if file.endswith('.zip'):
@@ -31,6 +31,10 @@ def start_worker(foldername):
 
 if __name__ == '__main__':
     db.init_connection()
-    student_name, percentage = start_worker(worker_path)
-    db.db_add_result(student_name, percentage)
+    while True:
+        if os.listdir(worker_path).count() != 0:
+            student_name, percentage = start_worker(worker_path)
+            db.db_add_result(student_name, percentage)
+        else:
+            continue
 
